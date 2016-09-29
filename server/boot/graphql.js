fd5c9071ec2876dad1d86773904c19620bb3381d
tallyb <tally.barak@gmail.com>
2016-09-29 15:42:55 +0300

    clean code

--- server/boot/graphql.js
+++ server/boot/graphql.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var apollo = require('apollo-server');
-var gqlTools = require ('graphql-tools');
-var bodyParser = require ('body-parser');
+var gqlTools = require('graphql-tools');
+var bodyParser = require('body-parser');
 
-var _ = require('lodash');
-var gqlSchema = require ('../graphql-schema.js');
+//var _ = require('lodash');
+var gqlSchema = require('../graphql-schema.js');
 
-module.exports = function(app) {
-  let typeDefs = [`
+module.exports = function (app) {
+    let typeDefs = [`
     scalar Date
     ${gqlSchema.generateEnums(app.models())}
     ${gqlSchema.generateTypeDefs(app.models())}
@@ -17,15 +17,15 @@ module.exports = function(app) {
       query: Query
     }
     `];
-  let schema = gqlTools.makeExecutableSchema ({
-    typeDefs,
-    resolvers: {},
-  });
+    let schema = gqlTools.makeExecutableSchema({
+        typeDefs,
+        resolvers: {}
+    });
 
-  let router = app.loopback.Router();
-  router.use('/graphql', bodyParser.json(), apollo.apolloExpress({schema: schema}));
-  router.use('/graphiql', apollo.graphiqlExpress({
-    endpointURL: '/graphql',
-  }));
-  app.use(router);
+    let router = app.loopback.Router();
+    router.use('/graphql', bodyParser.json(), apollo.apolloExpress({ schema: schema }));
+    router.use('/graphiql', apollo.graphiqlExpress({
+        endpointURL: '/graphql'
+    }));
+    app.use(router);
 };
