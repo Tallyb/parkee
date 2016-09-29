97d91908ce8030c1a0f4b98f09491d7c19086300
tallyb <tally.barak@gmail.com>
2016-09-29 15:39:06 +0300

    eslint and clean code

--- server/graphql-schema.js
+++ server/graphql-schema.js
@@ -2,52 +2,49 @@
 
 var _ = require('lodash');
 
-const mapEnums = (model) =>{
-  return _.map(model.definition.properties, (p, key) => {
-    return p.enum ? `enum ${key} { ${p.enum.join('\n ')} }` : '';
-  });
+const mapEnums = (model) => {
+    return _.map(model.definition.properties, (p, key) => {
+        return p.enum ? `enum ${key} { ${p.enum.join('\n ')} }` : '';
+    });
 };
 
 function generateEnums(models) {
-  return _.map(models, m => {
-    console.log (mapModel(m));
-    return mapEnums(m);
-  }).join ('\n');
-};
+    return _.map(models, m => {
+        return mapEnums(m);
+    }).join('\n');
+}
 
 const mapModel = (model) => {
-  const typesMapping = {
-    Number: 'Float',
-    String: 'String',
-    Boolean: 'Boolean',
-    ObjectID: 'ID',
-    Date: 'Date',
-  };
+    const typesMapping = {
+        Number: 'Float',
+        String: 'String',
+        Boolean: 'Boolean',
+        ObjectID: 'ID',
+        Date: 'Date'
+    };
 
-  let props = _.map (model.definition.properties, (p, key)  =>{
-    let req = p.required ? '!' : '';
-    let type = p.enum ? key : typesMapping[p.type.name];
-    return !p.deprecated ? `${key}: ${type}${req} ` : '';
-  });
+    let props = _.map(model.definition.properties, (p, key) => {
+        let req = p.required ? '!' : '';
+        let type = p.enum ? key : typesMapping[p.type.name];
+        return !p.deprecated ? `${key}: ${type}${req} ` : '';
+    });
 
-  return `type ${model.modelName} { ${props.join('\n ')} }`;
+    return `type ${model.modelName} { ${props.join('\n ')} }`;
 };
-
 
 function generateTypeDefs(models) {
-  return _.map(models, m => {
-    console.log (mapModel(m));
-    return mapModel(m);
-  }).join ('\n');
-};
+    return _.map(models, m => {
+        return mapModel(m);
+    }).join('\n');
+}
 
 function generateQueries(models) {
-  return _.map(models, m => {
-    return `${m.pluralModelName}: [${m.modelName}]`;
-  }).join('\n');
-};
+    return _.map(models, m => {
+        return `${m.pluralModelName}: [${m.modelName}]`;
+    }).join('\n');
+}
 module.exports = {
-  generateEnums,
-  generateQueries,
-  generateTypeDefs,
+    generateEnums,
+    generateQueries,
+    generateTypeDefs
 };
